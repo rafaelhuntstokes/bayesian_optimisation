4d731cadd2c15b0b2e829b418f3223a84653c38e
rafaelhuntstokes <rafaelhuntstokes@gmail.com>
2024-12-20 16:21:47 +0000

    script to clean directories after run

--- clean_files.sh
+++ clean_files.sh
@@ -7,3 +7,13 @@ rm plots/*.pdf
 rm plots/*.png
 rm plots/tres_comparison/*.png
 rm measured_points/*.npy
+rm dag_running/*
+rm condor_logs/residuals_errors/*.err
+rm condor_logs/residuals_outputs/*.out
+rm condor_logs/residuals_logs/*.log
+rm condor_logs/select_errors/*.err
+rm condor_logs/select_outputs/*.out
+rm condor_logs/select_logs/*.log
+rm condor_logs/sim_errors/*.err
+rm condor_logs/sim_outputs/*.out
+rm condor_logs/sim_logs/*.log
