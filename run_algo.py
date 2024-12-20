75ce38a3423a7ea9fd5f834fc649f6d7c00ddb2e
rafaelhuntstokes <rafaelhuntstokes@gmail.com>
2024-12-20 16:26:39 +0000

    Significant updates on creatin dag templates for algorithm running.

--- run_algo.py
+++ run_algo.py
@@ -2,16 +2,49 @@ import string
 
 """
 Script loads in the outer dag file and fills in the number of retries etc.
-
-Launches the optimisation!
 """
 
-NUM_ITERATIONS  = 10 # the number of times to optimise a single variable pair
-
+full_algo_repeats  = 2 # number of times entire tuning algo runs
+pair_block_repeats = 1 # number of times each block is repeated ([t1,t2,a1,a2], [t3, t4, a4,a3], [tr]) 
+sample_space_iters = 20 # number of times each parameter tuning loop is repeated within the blocks 
 # create the dag file wth max retries loaded in
 with open("dag_templates/main.dag", "r") as infile:
     raw_text = string.Template(infile.read())
-out_text = raw_text.substitute(NUM_ITERS = NUM_ITERATIONS)
-with open("main.dag", "w") as outfile:
+out_text = raw_text.substitute(NUM_ITERS = full_algo_repeats)
+with open("dag_running/main.dag", "w") as outfile:
     outfile.write(out_text)
 
+# create the dag file wth max retries loaded in
+with open("dag_templates/algo.dag", "r") as infile:
+    raw_text = string.Template(infile.read())
+out_text = raw_text.substitute(NUM_ITERS = pair_block_repeats, RISE_REPEATS = 0)
+with open("dag_running/algo.dag", "w") as outfile:
+    outfile.write(out_text)
+
+# create the dag file wth max retries loaded in
+with open("dag_templates/first_pair.dag", "r") as infile:
+    raw_text = string.Template(infile.read())
+out_text = raw_text.substitute(NUM_ITERS = sample_space_iters)
+with open("dag_running/first_pair.dag", "w") as outfile:
+    outfile.write(out_text)
+
+# create the dag file wth max retries loaded in
+with open("dag_templates/second_pair.dag", "r") as infile:
+    raw_text = string.Template(infile.read())
+out_text = raw_text.substitute(NUM_ITERS = sample_space_iters)
+with open("dag_running/second_pair.dag", "w") as outfile:
+    outfile.write(out_text)
+
+# create the dag file wth max retries loaded in
+with open("dag_templates/rise_time.dag", "r") as infile:
+    raw_text = string.Template(infile.read())
+out_text = raw_text.substitute(NUM_ITERS = sample_space_iters)
+with open("dag_running/rise_time.dag", "w") as outfile:
+    outfile.write(out_text)
+
+# create the dag file wth max retries loaded in
+with open("dag_templates/opto.dag", "r") as infile:
+    raw_text = string.Template(infile.read())
+out_text = raw_text.substitute()
+with open("dag_running/opto.dag", "w") as outfile:
+    outfile.write(out_text)
